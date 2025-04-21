c23b3e541d6357adc3de660df8fba5f0a44df223
mamqek <65137206+mamqek@users.noreply.github.com>
2025-04-21 16:50:49 +0200

    tailwind v4

---
+++ generators/app/templates/tailwind-v4/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin';
+import { fileURLToPath, URL } from 'node:url'
+import tailwind from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        laravel({
+            // for files which are loaded on every page (css), other page specific are in welcome.blade (but if removed from blade are not recognized, ask someone)
+            input: ["resources/js/app.js"],
+            refresh: true,
+        }),
+        tailwind(),  
+    ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url)), // Alias for Vue
+            '@plugins': fileURLToPath(new URL('./resources/js/plugins', import.meta.url)), // Alias for Vue
+            '@images': fileURLToPath(new URL('./public/images', import.meta.url)), // Alias for Vue
+        },
+    },
+})

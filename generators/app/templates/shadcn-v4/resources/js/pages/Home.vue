c057f04c78c2157037ae4e3ee9eab526f7e99ce8
mamqek <65137206+mamqek@users.noreply.github.com>
2025-04-21 16:51:06 +0200

    shadcn-vue for tailwind v4

---
+++ generators/app/templates/shadcn-v4/resources/js/pages/Home.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="content">
+
+    <img src="@images/logo.png" alt="Logo">
+
+    <p>{{ $t('welcome_to_dev') }}</p>
+
+    <p>{{ $t('welcome_description_1') }}</p>
+
+    <p>{{ $t('welcome_description_2') }}</p>
+
+    <br>
+    <p class="font-bold">Shadcn-vue component import example:</p>
+    <p>Download via: npx shadcn-vue@latest add switch</p>
+    <p>From https://www.shadcn-vue.com/docs/components/switch.html</p>
+    <Switch/>
+
+</div>
+</template>
+
+
+<script>
+
+import { Switch } from '@shadcn-vue-components/switch';
+
+export default {
+    name: 'Home',
+
+    components: {
+        Switch
+    },
+}
+</script>
+
+<style scoped>
+
+.content {
+    text-align: center;
+}
+
+    img {
+        width: 200px;
+    }
+
+</style>
